65b4fd3aded1860dc9ff8c28b5d24fcfcbf4ad54
王奕然 <744382100@qq.com>
2016-02-25 15:25:03 +0800

    refactor Resolve,separate responsibility

--- src/wangyiran/bean/Resolver.java
+++ src/wangyiran/bean/Resolver.java
@@ -6,6 +6,7 @@ package wangyiran.bean;
 
 import wangyiran.bean.annotation.InjectConstructor;
 import wangyiran.bean.tool.ClassDescription;
+import wangyiran.bean.tool.ConstructorResolver;
 import wangyiran.bean.tool.InjectPointConstructor;
 
 import java.lang.annotation.Annotation;
@@ -15,35 +16,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 所有resolver集合
+ * hold all resolvers
  */
 public class Resolver {
+    private ConstructorResolver constructorResolver;
+
+    public Resolver() {
+        this.constructorResolver = new ConstructorResolver();
+    }
+
     public CtorInjectPoint createCtorInjectPoint(Class beanClass) {
-        ClassDescription classDescription = ClassDescription.lookup(beanClass);
-        Constructor[] constructors = classDescription.getAllCtors();
-        List<ConstructorDescription> constructorDescriptions = new ArrayList<>(constructors.length);
-        Constructor annotationCtor = null;//注解的构造器
-        Constructor defaultCtor = null;
-        for (Constructor constructor : constructors){
-            Class[] parameterTypes = constructor.getParameterTypes();
-            if (parameterTypes.length == 0){
-                defaultCtor = constructor;
-            }
-            Annotation injectAnnotation = constructor.getAnnotation(InjectConstructor.class);
-            if (injectAnnotation != null){
-                annotationCtor = constructor;
-            }
-            ConstructorDescription constructorDescription = new ConstructorDescription(constructor,parameterTypes);
-            constructorDescriptions.add(constructorDescription);
-        }
-        if (annotationCtor == null){
-            annotationCtor = defaultCtor;
-        }
-
-        return createInjectConstructor(annotationCtor,annotationCtor.getParameterTypes());
+        return constructorResolver.resolveCtorInjectPoint(beanClass);
     }
 
-    private CtorInjectPoint createInjectConstructor(Constructor annotationCtor, Class[] parameterTypes) {
-        return new CtorInjectPoint(annotationCtor,parameterTypes);
-    }
+
 }
---
+++ src/wangyiran/bean/tool/ConstructorResolver.java
@@ -0,0 +1,44 @@
+package wangyiran.bean.tool;
+
+import wangyiran.bean.ConstructorDescription;
+import wangyiran.bean.CtorInjectPoint;
+import wangyiran.bean.annotation.InjectConstructor;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wangyiran on 25/2/2016.
+ */
+public class ConstructorResolver {
+    public CtorInjectPoint resolveCtorInjectPoint(Class beanClass) {
+        ClassDescription classDescription = ClassDescription.lookup(beanClass);
+        Constructor[] constructors = classDescription.getAllCtors();
+        List<ConstructorDescription> constructorDescriptions = new ArrayList<>(constructors.length);
+        Constructor annotationCtor = null;//注解的构造器
+        Constructor defaultCtor = null;
+        for (Constructor constructor : constructors){
+            Class[] parameterTypes = constructor.getParameterTypes();
+            if (parameterTypes.length == 0){
+                defaultCtor = constructor;
+            }
+            Annotation injectAnnotation = constructor.getAnnotation(InjectConstructor.class);
+            if (injectAnnotation != null){
+                annotationCtor = constructor;
+            }
+            ConstructorDescription constructorDescription = new ConstructorDescription(constructor,parameterTypes);
+            constructorDescriptions.add(constructorDescription);
+        }
+        if (annotationCtor == null){
+            annotationCtor = defaultCtor;
+        }
+
+        return createInjectConstructor(annotationCtor,annotationCtor.getParameterTypes());
+    }
+
+    private CtorInjectPoint createInjectConstructor(Constructor annotationCtor, Class[] parameterTypes) {
+        return new CtorInjectPoint(annotationCtor,parameterTypes);
+    }
+}
